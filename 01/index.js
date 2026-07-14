02da17c0fd11bd1154db6cf1d0229cd85d7e2143
Diego Araujo <dieggo_taylor@hotmail.com>
2026-07-14 14:09:23 -0300

    Criando um CRUD

--- 01/index.js
+++ 01/index.js
@@ -2,12 +2,52 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+//Query params = ?nome=NodeJS
+//Route params = /curso/2
+//Request Body = { nome: 'NodeJS', tipo: 'Backend'}
+
+//CRUD > Create, Read, Update, Delete
 const cursos = ['NodeJS', 'JavaScript', 'React Native'];
 
-server.get('/curso/:index', (req, res) => {
+//Listando Cursos
+server.get('/cursos', (req, res) => {
+   return res.json(cursos); 
+});
+
+//Buscando um curso pelo ID
+server.get('/cursos/:index', (req, res) => {
     const { index } = req.params;
 
     return res.json(cursos[index])
 });
+
+//Criando um novo curso
+server.post('/cursos', (req, res) => {
+    const { name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+});
+
+//Atualizando Cursos1
+server.put('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+});
+
+//Excluindo algum curso
+server.delete('/cursos/:index', (req,res) => {
+    const { index }  = req.params;
+
+    cursos.splice(index, 1);
+    return res.json(cursos);
+})
+
 
 server.listen(3000, () => console.log('Servidor online...'))
